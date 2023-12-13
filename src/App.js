8a5d0916fa1c2eb9b77da5d56d682a02d9218694
AM1G0S <suleymandzyazikov@gmail.com>
2023-12-13 19:06:55 +0300

    добавил сÐотображеÐние Ðсклелетонов до отображенÑподгрузки товаров и добавадаптировал сааблоки

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, {useEffect} from "react";
 import {Categories} from './components/Categories/Categories'
 import {Header} from './components/Header/Header'
 import {PizzaBlock} from './components/PizzaBlock/PizzaBlock'
+import {PizzaBlockSkeleton} from './components/PizzaBlock/Skeleton'
 import {Sort} from './components/Sort/Sort'
+
 import './scss/app.scss'
 
 export const App = () => {
 	const [products, setProducts] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
+	
 	
 	useEffect(() => {
 		fetch('https://65787cc6f08799dc80456b95.mockapi.io/items')
 			.then((response) => response.json())
-			.then((data) => setProducts(data))
+			.then((data) => {
+				setTimeout(() => {
+					setProducts(data)
+					setIsLoading(false)
+				}, 500)
+			})
 			.catch((error) => {
 				throw new Error('Произошла ошибка загрузки товаров', error);
 			});
 	}, []);
-	
-	console.log(products)
 	
 	return (
 		<div className='wrapper'>
@@ -32,7 +39,12 @@ export const App = () => {
 					
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{products.map((item) => <PizzaBlock key={item.id} {...item} />)}
+						{isLoading ? (
+							Array.from({ length: 12 }).map((_, index) => (
+								<PizzaBlockSkeleton key={index} />
+							))
+						) : ( products.map((item) => <PizzaBlock key={item.id} {...item} />)
+						)}
 					</div>
 				</div>
 			</div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+export const PizzaBlockSkeleton = (props) => (
+	<ContentLoader
+		class="pizza-block"
+		speed={1}
+		width={280}
+		height={460}
+		viewBox="0 0 280 460"
+		backgroundColor="#f3f3f3"
+		foregroundColor="#ecebeb"
+		{...props}
+	>
+		<circle cx="128" cy="125" r="125" />
+		<rect x="5" y="267" rx="5" ry="5" width="244" height="27" />
+		<rect x="7" y="307" rx="10" ry="10" width="244" height="86" />
+		<rect x="5" y="422" rx="5" ry="5" width="89" height="30" />
+		<rect x="115" y="412" rx="19" ry="19" width="136" height="46" />
+	</ContentLoader>
+)
