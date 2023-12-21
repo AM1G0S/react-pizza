ac795ef0758119db1716d8c62f8c9409bb2e05d5
AM1G0S <suleymandzyazikov@gmail.com>
2023-12-21 18:23:30 +0300

    добавил библиотеку redux-persist, теперь данные из стора хранятся в localStorage

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter} from "react-router-dom";
-import store from './redux/store'
+import store, {persistor} from './redux/store'
 import {Provider} from 'react-redux'
+import {PersistGate} from "redux-persist/integration/react";
 
 import {App} from './App'
 
 const rootElement = document.getElementById('root')
 
-if(rootElement) {
+if (rootElement) {
 	const root = ReactDOM.createRoot(rootElement)
 	root.render(
 		<BrowserRouter>
 			<Provider store={store}>
-				<App/>
+				<PersistGate persistor={persistor}>
+					<App/>
+				</PersistGate>
 			</Provider>
 		</BrowserRouter>
 	)
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,15 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import filter from './slices/filterSlice'
 import basket from './slices/basketSlice'
 import pizza from './slices/pizzaSlice'
 
-const store = configureStore({
-	reducer: {
-		filter,
-		basket,
-		pizza
-	},
+const rootReducer = combineReducers({
+	filter,
+	basket,
+	pizza
 })
+
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+})
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
